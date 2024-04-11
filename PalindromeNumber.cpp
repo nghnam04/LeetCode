79db3017e37041e78f413f8e2e45439bc4af5cc8
Nguyen Hoang Nam <hoangnam2004hp@gmail.com>
2024-04-11 23:55:57 +0700

    Sửa lại lỗi logic code và thêm comment

--- PalindromeNumber.cpp
+++ PalindromeNumber.cpp
@@ -1,17 +1,26 @@
 #include<iostream>
+
 using namespace std;
-bool isPalindrome(int x){
-if(x<0 || (x!=0 && x%10==0)) return false;
-int temp=0;
-while(x>temp){
-    temp=(temp*10)+(x%10);
-    x%10;
+//Hàm kiểm tra xem 1 số khi reverse có chính là số đấy không
+bool isPalindrome(int x){ 
+if(x<0) return false;
+
+long int temp=0, reverse = x, remainNumber; //Biến reverse để lưu lại gtri x ban đầu
+while(x!=0){ //Reverse 1 số
+remainNumber=x%10;
+    temp=temp*10 + remainNumber;
+    x/=10;
 }
-return x==temp;
+return reverse == temp;
 }
 int main(){
     int a;
     cin >> a;
-    cout << isPalindrome(a);
+     if (isPalindrome(a)) {
+        cout << a << " is a palindrome." << endl;
+    } else {
+        cout << a << " is not a palindrome." << endl;
+    }
+    
     return 0;
 }
