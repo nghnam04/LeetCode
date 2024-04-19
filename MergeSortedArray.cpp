8bb341584a389c6646895d5a790c2dbf3b17d88c
Nguyen Hoang Nam <hoangnam2004hp@gmail.com>
2024-04-19 21:57:55 +0700

    Comment rõ hơn phần sắp xếp mảng tăng dần

--- MergeSortedArray.cpp
+++ MergeSortedArray.cpp
@@ -1,8 +1,7 @@
-#include<iostream>
-#include<algorithm>
-#include<vector>
+#include<bits/stdc++.h>
 using namespace std;
 
+//Hàm trộn 2 mảng và sắp xếp theo thứ tự tăng dần
   void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         for (int j = 0, i = m; j<n; j++){
             nums1[i] = nums2[j];
