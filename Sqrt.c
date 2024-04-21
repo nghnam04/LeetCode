b8db3dd0621bf2e852044f2e8fc9de464afb0d63
Nguyen Hoang Nam <hoangnam2004hp@gmail.com>
2024-04-21 22:00:58 +0700

    Tính căn bậc hai của một số

---
+++ Sqrt.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+//Hàm tính căn bậc hai của một số, làm tròn chỉ lấy phần nguyên
+int mySqrt(int x) {
+     if (x == 0)
+            return x;
+        int first = 1, last = x;
+        while (first <= last) {
+            int mid = first + (last - first) / 2;
+            if (mid  == x / mid)
+                return mid;
+            else if (mid > x / mid) {
+                last = mid - 1;
+            }
+            else {
+                first = mid + 1;
+            }
+        }
+        return last;
+}
+int main(){
+    int x;
+    scanf("%d", &x);
+    printf("%d", mySqrt(x));
+    return 0;
+}
