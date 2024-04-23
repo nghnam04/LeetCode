d9cba67f3d60e55badd3c4390a38db74fb5e22a0
Nguyen Hoang Nam <hoangnam2004hp@gmail.com>
2024-04-23 23:17:53 +0700

    Bài toán trộn 2 danh sách theo thứ tự không giảm các số

---
+++ MergeTwoLists.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+  struct ListNode {
+      int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+  //Hàm trộn 2 lists
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* temp1 = list1;
+        ListNode* temp2 = list2;
+        if(list1 == NULL) return list2;
+        if(list2 == NULL) return list1;
+        if(list1->val < list2->val){
+            list1->next = mergeTwoLists(list1->next, list2);
+            return list1;
+        }
+        else{
+            list2->next = mergeTwoLists(list1, list2->next);
+            return list2;
+        }
+    }
+};
